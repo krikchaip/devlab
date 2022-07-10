3d5687b7952af6f2c3e176b78784dd8f1271be7b
krikchaip <krikchai.p@gmail.com>
2022-07-11 03:08:26 +0700

    🧹💨

--- vite-xstate/src/app/index.tsx
+++ vite-xstate/src/app/index.tsx
@@ -3,17 +3,10 @@ import { useMachine } from "@xstate/react";
 import { clock } from "~/machines";
 
 export default function App() {
-  const [state, send] = useMachine(clock, {
-    actions: {
-      rotateSecondHand: (ctx, evt) => console.log(ctx.secondHandDegree, evt),
-      rotateMinuteHand: (ctx, evt) => console.log(ctx.minuteHandDegree, evt),
-      rotateHourHand: (ctx, evt) => console.log(ctx.hourHandDegree, evt),
-    },
-  });
-
+  const [state, send] = useMachine(clock);
   return (
     <div>
-      <button onClick={() => send({ type: "TURN_OFF" })}>TURN_OFF</button>
+      <button onClick={() => send("TURN_OFF")}>TURN_OFF</button>
       <button onClick={() => send("TURN_ON")}>TURN_ON</button>
       <pre style={{ display: "block" }}>{state.can("TURN_OFF").toString()}</pre>
     </div>
--- vite-xstate/src/machines/clock.ts
+++ vite-xstate/src/machines/clock.ts
@@ -7,42 +7,53 @@ export const clockContext = {
   hourHandDegree: 0,
 };
 
+export type ClockEvent = { type: "TURN_ON" } | { type: "TURN_OFF" };
+
 export default /** @xstate-layout N4IgpgJg5mDOIC5QGMA2B7ZBrAdOgZvgMQAqAqgEoByA+gPJWKgAO6sAlgC7voB2TIAB6IAjADYArDgCcs2RJEAOACyrpYgMwAaEAE9EAJmXScx2YrEjpigOwGADCJsBfZzrSZcfHOwiowRIKwnACGnGA4IfjhAE4AFCL2SQCURB7YeLw+fmACrBzcfALCCGo4BuoWGspOdprKOvoIFQY44mKSGjb2ymLGru4YGd7c2ER5bFw8-EhCiMY2ODaKIgZiioqOErJijYaqbR19iganihISXQMg6V68pJS0dABizxMF08WikjJy0goqNSaPbNRymP6JDROAwrESuNwgXjoCBwAS3PCEd5TIqzErKAwghytRQaUkGERQsQGLrU67o7y+fxYwozUAlDQGKQVdYKGEw0m7PT7KRmawaDrKGySAwuBH0rKjLDMz64xBiMGJaTi7bSBw2Qn2MS-cz1DT-HqywaeTLKnFsxAaMHc86rE4k8UgikaY2yPkiEQSFRiOlDJWzfLY1lzBBKQnev4Jv4WeHOIA */
-createMachine<ClockContext>({
-  id: "clock",
-  description: "clock signals simulation",
-  context: clockContext,
-  initial: "off",
-  states: {
-    off: {
-      on: {
-        TURN_ON: {
-          target: "on",
+createMachine<ClockContext, ClockEvent>(
+  {
+    id: "clock",
+    description: "clock signals simulation",
+    context: clockContext,
+    initial: "off",
+    states: {
+      off: {
+        on: {
+          TURN_ON: {
+            target: "on",
+          },
         },
       },
-    },
-    on: {
-      initial: "idle",
-      states: {
-        idle: {
-          after: {
-            "1000": {
-              target: "tick",
+      on: {
+        initial: "idle",
+        states: {
+          idle: {
+            after: {
+              "1000": {
+                target: "tick",
+              },
+            },
+          },
+          tick: {
+            entry: ["rotateSecondHand", "rotateMinuteHand", "rotateHourHand"],
+            always: {
+              target: "idle",
             },
           },
         },
-        tick: {
-          entry: ["rotateSecondHand", "rotateMinuteHand", "rotateHourHand"],
-          always: {
-            target: "idle",
+        on: {
+          TURN_OFF: {
+            target: "off",
           },
-        },
-      },
-      on: {
-        TURN_OFF: {
-          target: "off",
         },
       },
     },
   },
-});
+  {
+    actions: {
+      rotateSecondHand: (ctx, evt) => console.log(ctx.secondHandDegree, evt),
+      rotateMinuteHand: (ctx, evt) => console.log(ctx.minuteHandDegree, evt),
+      rotateHourHand: (ctx, evt) => console.log(ctx.hourHandDegree, evt),
+    },
+  }
+);
