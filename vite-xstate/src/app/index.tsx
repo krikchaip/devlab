04eaca0365d79b97601cb3cd936953b8be86219e
krikchaip <krikchai.p@gmail.com>
2022-07-15 01:54:47 +0700

    vite-xstate: clock foundation

--- vite-xstate/src/app/index.tsx
+++ vite-xstate/src/app/index.tsx
@@ -5,10 +5,38 @@ import { clock } from "~/machines";
 export default function App() {
   const [state, send] = useMachine(clock);
   return (
-    <div>
-      <button onClick={() => send("TURN_OFF")}>TURN_OFF</button>
-      <button onClick={() => send("TURN_ON")}>TURN_ON</button>
-      <pre style={{ display: "block" }}>{state.can("TURN_OFF").toString()}</pre>
+    <div className="absolute-center space-y-5">
+      <div
+        className="
+          w-[25rem] h-[25rem] rounded-full border-8 relative
+          before:w-1.5 before:h-1.5 before:absolute-center before:bg-black before:rounded-full
+        "
+      >
+        {Array.from(Array(12), (_, i) => (
+          <div
+            key={i}
+            className="
+              w-0.5 h-[calc(12.5rem-8px)] bg-black absolute-center !top-0 origin-bottom
+              [clip-path:inset(0_0_calc(100%-theme(spacing.2)))]
+            "
+            style={{ transform: `translateX(-50%) rotate(${i * 30}deg)` }}
+          />
+        ))}
+      </div>
+      <div className="space-x-2 flex justify-center">
+        <button
+          className="p-2 rounded border border-solid border-red-500 hover:bg-red-200 transition"
+          onClick={() => send("TURN_OFF")}
+        >
+          TURN_OFF
+        </button>
+        <button
+          className="p-2 rounded border border-solid border-red-500 hover:bg-red-200 transition"
+          onClick={() => send("TURN_ON")}
+        >
+          TURN_ON
+        </button>
+      </div>
     </div>
   );
 }
